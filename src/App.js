8852026bb861ddf2e07b0d2c7f2667ea4f0f330f
advika31 <advikasinghal31@gmail.com>
2024-11-21 13:17:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './navbar';
+import About from './pages/about';
+import Home from './pages/home';
+import Pricing from './pages/pricing';
+import Contact from './pages/contact';
+import SignIn from './pages/signin.js';
+import { Route , Routes } from "react-router-dom"
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar />
+    <div className='container'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
     </div>
-  );
+    
+    </>
+  )
 }
 
-export default App;
+export default App
